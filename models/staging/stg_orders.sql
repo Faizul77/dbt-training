d453ca3b77ec9085d4133b08a5b07c2309a6bf71
faizullah ashraf <myclouddoorsfa@gmail.com>
2023-10-26 12:02:12 +0000

    develop delivery_team.CSV and added to stg_orders

--- models/staging/stg_orders.sql
+++ models/staging/stg_orders.sql
@@ -18,9 +18,12 @@ category,
 productname,
 subcategory,
 ordersellingprice2 - ordercostprice as orderprofit 
-{{markup('ordersellingprice2','ordercostprice')}} as markup
+{{markup('ordersellingprice2','ordercostprice')}} as markup,
+d.delivery_team
 from {{ ref('raw_orders') }} as o
 left join {{ ref('raw_customer') }} as c
 on o.customerid = c.customerid
 left join {{ ref('raw_product') }} as p
 on o.productid = p.productid
+left join {{ ref('delivery_team') }} as d
+on o.shipmode = d.delivery_team
